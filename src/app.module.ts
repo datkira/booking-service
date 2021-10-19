7b86fa089c587f3c141191d9534002ee332e7d96
Nguyen Tien Dat <abc.nguyentiendat@gmail.com>
2021-10-19 20:33:02 +0700

    update config and mapper

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { SharedModule } from './shared/shared.module';
+import { ConfigurationService } from './shared/configuration/configuration.service';
+import { Configuration } from './shared/configuration/configuration.enum';
 
 @Module({
-  imports: [],
+  imports: [SharedModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule {
+  static host: string;
+  static port: number | string;
+  static isDev: boolean;
+
+  constructor (private readonly _configurationService: ConfigurationService) {
+    console.log(_configurationService.get(Configuration.PORT))
+    AppModule.port = AppModule.normalizePort(_configurationService.get(Configuration.PORT));
+    AppModule.host = _configurationService.get(Configuration.HOST);
+    AppModule.isDev = _configurationService.isDevelopment;
+  }
+
+  private static normalizePort (param: number | string): number | string {
+    const portNumber: number = typeof param === 'string' ?
+      parseInt(param, 10) : param;
+
+    if (isNaN(portNumber)) return param;
+    else if (portNumber >= 0) return portNumber;
+
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+declare const module: any;
+
+async function bootstrap () {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
+  console.log(AppModule.port)
+  await app.listen(AppModule.port);
 }
+
 bootstrap();
